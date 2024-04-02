f89a093b722f62a66b26c140d7d43ccb97003d92
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2024-04-02 14:56:46 +0000

    Add createNotification function to notificationController.js and update server.js

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,9 +1,27 @@
-const Notification = require('../models');
-
 exports.createNotification = async (req, res) => {
-  try {
-    
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
+    try {
+      const { message } = req.body;
+      let responseMessages = [];
+  
+      message.forEach((msg) => {
+        if (msg.sms && msg.phoneNumber) {
+          // Custom action for SMS
+          responseMessages.push(`SMS submitted for processing ${msg.phoneNumber}: ${msg.sms}`);
+        }
+  
+        if (msg.email && msg.emailAddress) {
+          // Custom action for Email
+          responseMessages.push(`Email submitted for processing ${msg.emailAddress}: ${msg.email}`);
+        }
+  
+        if (msg.push && msg.deviceType) {
+          // Custom action for Push
+          responseMessages.push(`Push notif submitted for processing ${msg.deviceType}: ${msg.push}`);
+        }
+      });
+  
+      res.status(200).json({ responseMessages: responseMessages });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  };
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const notificationRoutes = require('./routes/notificationRoutes');
 
+app.use(express.json()); // for parsing application/json
+app.use('/', notificationRoutes);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
